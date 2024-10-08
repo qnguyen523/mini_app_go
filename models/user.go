60d84e7f58ea220eb88c668741fc4662d0f82a02
Quoc Nguyen <qnguyen523@gmail.com>
2024-10-08 21:24:36 +0700

    feature: set up user model

---
+++ models/user.go
@@ -0,0 +1,10 @@
+package models
+
+// import "time"
+
+type User struct {
+	ID       uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name     string `json:"name"`
+	Email    string `gorm:"unique" json:"email"`
+	Password []byte `json:"-"`
+}
