bd103bfc8839748d112979107ab696ff158dab1c
Quoc Nguyen <qnguyen523@gmail.com>
2024-11-03 19:16:40 +0700

    feature: simple app

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request received")
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+}
